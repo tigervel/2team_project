e0af15f8ea64c08eecc9340f958aaf199559b840
tigervel <silpy.jdh5057@gmail.com>
2025-07-31 18:27:48 +0900

    견저서 작성 페이지

--- front/src/pages/OrderPage.js
+++ front/src/pages/OrderPage.js
@@ -1,6 +1,24 @@
+import { Box } from "@mui/material"
+import OrderComponent from "../layout/component/OrderComponent"
+
 const OrderPage = ()=>{
     return(
-        <div>여기는 이제 주문 접수 페이지야</div>
-    )
-}
+            <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center", // 가로 가운데 정렬
+        width: "100%",
+        minHeight: "100vh", // 높이 최소 확보
+        backgroundColor: "#f9f9f9", // 배경 확인용 (선택)
+        pt: 4,
+      }}
+    >
+      <Box sx={{ width: "100%", maxWidth: 1200, px: 2 }}>
+        <OrderComponent />
+      </Box>
+    </Box>
+  );
+};
+    
+
 export default OrderPage
