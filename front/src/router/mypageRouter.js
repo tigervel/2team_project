575d7d33a854eac5e9c9fc2a49fecff1cf4c09ea
LeeJaeJun <fullstack.juni7015@gmail.com>
2025-08-01 17:39:08 +0900

    router fix

--- front/src/router/mypageRouter.js
+++ front/src/router/mypageRouter.js
@@ -1,8 +1,29 @@
-const mypageRouter = () =>{
-    return[
-        {
-            path:"/"
-        }
-    ]
-}
-export default mypageRouter
+import { lazy, Suspense } from "react";
+
+const Loading = <div>Loading 중....</div>;
+
+const MyInform = lazy(() => import("../pages/mypage/MyInform"));
+const EditMyInform = lazy(() => import("../pages/mypage/EditMyInform"));
+const EditVehicleInform = lazy(() => import("../pages/mypage/EditVehicleInform"));
+const DeliveryInform = lazy(() => import("../pages/mypage/DeliveryInform"));
+
+const mypageRouter = [
+  {
+    index: true,
+    element: <Suspense fallback={Loading}><MyInform /></Suspense>
+  },
+  {
+    path: "edit",
+    element: <Suspense fallback={Loading}><EditMyInform /></Suspense>
+  },
+  {
+    path: "vehicle",
+    element: <Suspense fallback={Loading}><EditVehicleInform /></Suspense>
+  },
+  {
+    path: "delivery",
+    element: <Suspense fallback={Loading}><DeliveryInform /></Suspense>
+  }
+];
+
+export default mypageRouter;
