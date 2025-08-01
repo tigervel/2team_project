bdcb5b46dd17f56d7b78dea77badda7af4a5d9e9
tigervel <silpy.jdh5057@gmail.com>
2025-08-01 17:06:21 +0900

    백엔드 테스트

--- backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -3,12 +3,18 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.giproject.dto.estimate.EstimateDTO;
+
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 			System.out.println("1수정");
 	}
-
+	@Test
+	void test() {
+		EstimateDTO dto= EstimateDTO.build()
+				
+	}
 }
