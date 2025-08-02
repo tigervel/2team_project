6896f8750bdc35b49e9e454483e3198da238f915
Shawn <hoon4407@naver.com>
2025-08-03 01:09:34 +0900

    QAboard
    package.json
    yarn 패키지 매니저와 의존성 충돌
    react-router-dom 6.30.1 로 다운그레이드
    단순 복사 붙여넣기

---
+++ front/src/pages/qaboard/qaboardPage.tsx
@@ -0,0 +1,11 @@
+import QABoard from "@/components/QABoard";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <QABoard />
+    </div>
+  );
+};
+
+export default Index;
