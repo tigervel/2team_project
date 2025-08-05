8622028fba5db2e737d80dc72c99925a62dcb753
tigervel <silpy.jdh5057@gmail.com>
2025-08-05 11:23:46 +0900

    어드민 경로수정

--- front/src/pages/AdminPage.js
+++ front/src/pages/AdminPage.js
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
-const AdminPage = ()=>{
-    return(
-        <div><Outlet/></div>
-    )
-}
-export default AdminPage
+import { Box } from "@mui/material";
+import AdminSidebar from "../common/AdminSidebar";
+
+const AdminPage = () => (
+  <Box sx={{ display: "flex", height: "100vh" }}>
+    <AdminSidebar />
+    <Box component="main" sx={{ flexGrow: 1, p: 3, backgroundColor: "#f3f4f6", overflow: "auto" }}>
+      <Outlet />
+    </Box>
+  </Box>
+);
+
+export default AdminPage;
