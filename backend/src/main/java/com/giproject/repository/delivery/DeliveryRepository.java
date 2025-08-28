f18a14da50d6541ae73266a9230a3523dd2981ae
LeeJaeJun <fullstack.juni7015@gmail.com>
2025-08-28 11:09:29 +0900

    Merge

--- backend/src/main/java/com/giproject/repository/delivery/DeliveryRepository.java
+++ backend/src/main/java/com/giproject/repository/delivery/DeliveryRepository.java
@@ -1,9 +1,16 @@
 package com.giproject.repository.delivery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 import com.giproject.entity.delivery.Delivery;
+import com.giproject.entity.payment.Payment;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-
+	Optional<Delivery> findByPayment_PaymentNo(Long paymentNo);
+	@Query("select p from Payment p where p.orderSheet.orderNo = :orderNo")
+	Optional<Payment> findByOrderSheet_OrderNo(@Param("orderNo") Long orderNo);
 }
