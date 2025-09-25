5c0504d2257bdfd74dd17866c62793f451b96b4d
tigervel <silpy.jdh5057@gmail.com>
2025-09-25 17:46:52 +0900

    Revert "estimate 추가 및 api분리 feesExtraApi추가"
    This reverts commit d673c728a1c5b4406fa16912430aa8c3168e009c.

--- flutter/flutterproject/lib/API/SimpleAPI.dart
+++ flutter/flutterproject/lib/API/SimpleAPI.dart
@@ -1,11 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutterproject/API/ApiConfig.dart';
 import 'package:flutterproject/DTO/FeesDTO.dart';
-import 'package:flutterproject/DTO/FeesExtraDTO.dart';
-import 'package:flutterproject/Model/FeesExtraModel.dart';
 import 'package:flutterproject/Model/FeesModel.dart';
 
-class FeesApi {
+class SimpleAPI {
   final Dio  _dio = Dio(BaseOptions(
     baseUrl: Apiconfig.baseUrl,
     headers: {"Content-Type": "application/json"},
@@ -15,12 +13,5 @@ class FeesApi {
     final response = await _dio.post("/g2i4/estimate/subpath/searchfeesbasic");
     final List<dynamic> data = response.data;
     return data.map((e) => FeesDTO.fromJson(e).toModel()).toList();
-  }
-
-  Future<List<FeesExtraModel>> fetchFeesExtra() async {
-    final response = await _dio.post('/g2i4/estimate/subpath/searchfeesextra');
-    final List<dynamic> data =response.data;
-    return data.map((e) => FeesExtraDTO.fromJson(e).toModel()).toList();
-
   }
 }
